6646a117340da74063d98a5465159bbbed4c2787
Narotam-Mishra <narotam21jan@gmail.com>
2024-01-14 23:52:48 +0530

    page-view-app_1.2

--- src/components/PageEventViewTable.jsx
+++ src/components/PageEventViewTable.jsx
@@ -1,10 +1,30 @@
 import axios from 'axios';
 import { useState } from 'react';
 import '../styles/PageViewTable.css'
+import cuid from 'cuid';
+import { loremIpsum } from 'lorem-ipsum';
+import { randomDate } from '../utilities/randomDate';
+
 
 const PageEventViewTable = () => {
   const [pageViews, setPageViews] = useState([]);
   const [error, setError] = useState(null);
+
+  const upEventData = {
+    id: cuid(),
+    created_at: new Date(),
+    page: {
+      title: loremIpsum(),
+      description: loremIpsum({ count: 3 }),
+      tags: Array.from({ length: Math.floor(Math.random() * 10) }, () =>
+        loremIpsum({ units: 'words', count: 1 })
+      ),
+    },
+    user: {
+      id: `USER${cuid()}`,
+      created_at: randomDate(new Date(2019, 0, 1), new Date()),
+    },
+  };
 
   const fetchPageViews = async () => {
     try {
@@ -14,6 +34,18 @@ const PageEventViewTable = () => {
     } catch (error) {
       console.error('Error fetching page views:', error);
       setError('Error fetching page views. Please try again.'); // Set error message
+    }
+  };
+
+  const updateTableData = async () => {
+    try {
+      // Call the API endpoint to update all data
+      await axios.patch('http://localhost:7373/api/v1/pageView', upEventData);
+      console.log('All data updated successfully');
+      // After updating, fetch the updated data
+      fetchPageViews();
+    } catch (error) {
+      console.error('Error updating all data:', error);
     }
   };
 
@@ -54,6 +86,7 @@ const PageEventViewTable = () => {
           </table>
         )
       )}
+      { pageViews.length > 0 && <button onClick={updateTableData} className='updateBtn'>Update Table Data</button>}
     </div>
   );
 };
